5f056d659c44a63d6a20594f2ed7da18d7cdc182
Jay Shah <89582065+jayshah-12@users.noreply.github.com>
2024-08-13 16:47:05 +0530

    Update scrape.py

--- scrape.py
+++ scrape.py
@@ -1,23 +1,26 @@
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-import os
-from datetime import datetime
-
-# Login credentials
-usernames = ["jayshah36262@gmail.com"]
-passwords = ["Jayshah12"]
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 
 def login_and_download_file(url, username, password, file_suffix):
-    driver = webdriver.Chrome()
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    
+    # Connect to the Selenium Grid
+    driver = webdriver.Remote(
+        command_executor='http://selenium:4444/wd/hub',
+        options=chrome_options
+    )
+    
     driver.maximize_window()
     driver.get(url)
 
     try:
         # Log in
         WebDriverWait(driver, 10).until(
-            EC.element_to_be_clickable((By.XPATH, '//*[contains(concat( " ", @class, " " ), concat( " ", "account", " " ))]'))
+            EC.element_to_be_clickable((By.XPATH, '//*[contains(concat(" ", @class, " "), " account ")]'))
         ).click()
 
         email_input = WebDriverWait(driver, 10).until(
@@ -31,11 +34,11 @@ def login_and_download_file(url, username, password, file_suffix):
         password_input.send_keys(password)
 
         WebDriverWait(driver, 10).until(
-            EC.element_to_be_clickable((By.XPATH, '//*[contains(concat( " ", @class, " " ), concat( " ", "icon-user", " " ))]'))
+            EC.element_to_be_clickable((By.XPATH, '//*[contains(concat(" ", @class, " "), " icon-user ")]'))
         ).click()
 
         # Navigate to the desired page
-        driver.get("https://www.screener.in/company/FACT/")
+        driver.get("https://www.screener.in/company/RELIANCE/consolidated/")
         
         # Wait for the download button to be clickable and click it
         download_button = WebDriverWait(driver, 10).until(
@@ -48,7 +51,3 @@ def login_and_download_file(url, username, password, file_suffix):
 
     finally:
         driver.quit()
-
-if __name__ == '__main__':
-    for i, (username, password) in enumerate(zip(usernames, passwords)):
-        login_and_download_file("https://www.screener.in/", username, password, i)
