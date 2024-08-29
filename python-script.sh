a8a08502b4cb3293d10242d0d45db668f4cd0f11
Jay Shah <89582065+jayshah-12@users.noreply.github.com>
2024-08-29 10:37:29 +0530

    Update python-script.sh

--- python-script.sh
+++ python-script.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
+
+
+set -a
+. secrets-output/secrets.env
+set +a
+ls secrets-output/secrets.env
+
+
 cd python-script-repo
 # Install necessary Python libraries
 apt-get update
@@ -6,14 +14,6 @@ apt-get install -y wget
 pip install --upgrade pip
 pip install -r requirements.txt
 
-cd ..
-# Source the environment variables from the secrets file
-set -a
-. secrets-output/secrets.env
-set +a
-ls secrets-output/secrets.env
-# Change directory to where the Python script is located
-cd python-script-repo
 
 # Run the Python script with environment variables
 python test.py
