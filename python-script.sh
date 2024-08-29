464f52e3f943985ad10ccceb43c4240bd43b836b
Jay Shah <89582065+jayshah-12@users.noreply.github.com>
2024-08-29 10:30:31 +0530

    Update python-script.sh

--- python-script.sh
+++ python-script.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 # Install necessary Python libraries
-# apt-get update
-# apt-get install -y wget
-# pip install --upgrade pip
+apt-get update
+apt-get install -y wget
+pip install --upgrade pip
 pip install -r requirements.txt
 
 
